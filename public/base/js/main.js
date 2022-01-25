4dde9719813532c4d5dd4f2c180c9d0b1901b32f
cpz <214564605@qq.com>
2022-01-25 17:55:36 +0800

    基础 ui框架 demo 0.10

--- public/base/js/main.js
+++ public/base/js/main.js
@@ -2,9 +2,32 @@
 let main={
     debug:true,//记录开始时间
     start_time:Date.now(),//记录开始时间
-    //常用方法
-    parent:function (){
 
+    //常用方法
+    parents:function (el,selector){
+        let _this=this;
+        let selector_array=selector.split(',');
+        let return_parent=[];
+        let parent=el.parentElement;
+        selector_array.forEach(function (selector_string){
+            let index=0;
+            while (true){
+                if(parent==null)return false;
+                if(return_parent.length>=1)return true;
+                if(index>=100)return false;
+                let temp_str=selector_string;
+                let type='tag';
+                if(temp_str[0]=="#")type='id';//ID选择器
+                if(temp_str[0]==".")type='class';//class选择器
+                if(temp_str[0]=="#" || temp_str[0]==".")temp_str=temp_str.substr(1);
+                if(type=='id' && temp_str==parent.id)return_parent.push(parent);
+                if(type=='tag' && temp_str==parent.nodeName.toLowerCase())return_parent.push(parent);
+                if(type=='class' && temp_str==parent.className)return_parent.push(parent);
+                parent=parent.parentElement;
+                index+=1;
+            }
+        })
+        return return_parent;
     },
     run_time:function (msg){
         let run_time=Date.now()-this.start_time;
@@ -21,16 +44,31 @@ let main={
     load:function (){
         if(this.debug)this.run_time('事件->'+'load(整个页面加载)')
     },
+
     //左侧菜单
     left_menu_module:function (){
         let path_name=location.pathname;//得到路径名称
         let item_group_list=document.querySelectorAll(".main_menu>.ui_menu_item_group>.ui_menu_item_group");
+        let _this=this;
         item_group_list.forEach(function (item_group){
             let link_list=item_group.querySelectorAll(".ui_menu_item_group .ui_menu_item .ui_menu_title_content a");
             link_list.forEach(function (link){
+                let parent=_this.parents(link,'.ui_menu_item');
+                let item=null;
+                if(parent.length>=1)item=parent[0];
+                if(item!=null){
+                    window.cca=item.classList;
+
+                    item.classList.remove('ui_menu_item_selected');
+                }
+
                 let is_exist=link.getAttribute('href').indexOf(path_name);
                 if(is_exist>=0){
-                    link.parentElement.parentElement.classList.add('ui_menu_item_selected');
+                    if(item.classList.indexOf('ui_menu_item_selected')>=0){
+                        item.classList.add('ui_menu_item_selected');
+                    }else{
+                        item.classList.remove('ui_menu_item_selected');
+                    }
                 }
             })
         })
